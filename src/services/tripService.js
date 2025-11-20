cf0fbaf910d1dbc55cbd93c91704c34d1526c881
Erick <erickloboabreu@hotmail.com>
2025-11-20 15:57:16 +0100

    create createTrip service

---
+++ src/services/tripService.js
@@ -0,0 +1,21 @@
+import { prisma } from '../lib/prisma.js'
+import { createTripSchema, updateTripSchema } from '../modules/trip.schema.js'
+
+export const createTrip = async (input) => {
+  const validateInput = createTripSchema.safeParse(input)
+
+  if (validateInput.success) {
+    const { title, description, startDate, endDate } = validateInput.data
+    const trip = await prisma.trip.create({
+      data:{
+        title,
+        description,
+        startDate,
+        endDate
+      }
+    })
+    return trip
+  } else {
+    throw new Error(validateInput.error?.message || 'Invalid trip input')
+  }
+}
