b2f6391ee8fc41f86982243eb2fcb4361a38dec5
Erick <erickloboabreu@hotmail.com>
2025-11-20 15:44:00 +0100

    create createTripSchema and updateTripSchema zod

---
+++ src/modules/trip.schema.js
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+export const createTripSchema = z.object({
+  title: z.string().min(1, 'Title cannot be empty'),
+  description: z.string().optional(),
+  startDate: z.coerce.date(),
+  endDate: z.coerce.date().optional()
+})
+export const updateTripSchema = z.object({
+  title: z.string().optional(),
+  description: z.string().optional(),
+  startDate: z.coerce.date().optional(),
+  endDate: z.coerce.date().optional()
+})
