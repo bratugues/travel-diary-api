a8e49d246d98d57f05d39a23f7d0d8f45bbdc9b4
Erick <erickloboabreu@hotmail.com>
2025-12-01 13:47:36 +0100

    add upload middleware to entries routes

--- src/routes/entryRoutes.js
+++ src/routes/entryRoutes.js
@@ -1,17 +1,18 @@
 import { Router } from 'express'
 import { deleteEntryController, createEntryController, updateEntryController, getEntryByIdController, listEntriesByTripIdController } from '../modules/entry/entry.controller'
 import { protect } from '../middlewares/authMiddleware'
+import { upload } from '../middlewares/uploadMiddleware'
 
 
 const router = Router()
 
-router.post('/trips/:tripId/entries', protect, createEntryController)
+router.post('/trips/:tripId/entries', protect, upload.single('image'), createEntryController)
 
-router.get('/trips/:tripId/entries', protect, listEntriesByTripIdController)
+router.get('/trips/:tripId/entries', protect, upload.single('image'), listEntriesByTripIdController)
 
-router.get('/entries/:id', protect, getEntryByIdController)
+router.get('/entries/:id', protect, upload.single('image'), getEntryByIdController)
 
-router.patch('/entries/:id', protect, updateEntryController)
+router.patch('/entries/:id', protect, upload.single('image'), updateEntryController)
 
-router.delete('/entries/:id', protect, deleteEntryController)
+router.delete('/entries/:id', protect, upload.single('image'), deleteEntryController)
 export const entryRouter = router
