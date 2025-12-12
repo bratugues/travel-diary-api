4af664b5715049e4adf4899181b15f81802fa4ad
Erick <erickloboabreu@hotmail.com>
2025-12-12 11:25:20 +0100

    Fix: Redirect root path to login

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
       <Toaster richColors position='top-right' />
       <Routes>
 
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={
